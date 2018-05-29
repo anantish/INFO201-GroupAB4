a96a30051ec29d178ca5efbd0cc133d500c7c58f
Olena Gavrishchuk <olenagav@uw.edu>
2018-05-28 21:17:38 -0700

    Added Seattle map

--- seattle_map_code/app.R
+++ seattle_map_code/app.R
@@ -6,13 +6,14 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(maps)
+library(leaflet)
+library(ggmap)
 
 ui <- fluidPage()
 
 server <- function(input, output) {
-  seattle_map <- ggplot(data = emissions_data) +
-    geom_polygon(mapping = aes(x = long, y = lat, group = group,
-                               fill = emissions_catergories))
+
+  
 }
 
 shinyApp(ui = ui, server = server)
---
+++ seattle_map_code/seattle_map.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(maps)
+library(leaflet)
+library(ggmap)
+library(sp)
+library(geojsonio)
+
+seattle_boundaries <- geojson_read("census-tracts.geojson", what = "sp")
+
+data <- read.csv(file = "../data/SPD_Officer_Involved_Shooting__OIS__Data.csv", 
+                 stringsAsFactors = FALSE)
+data$Longitude <- as.numeric(data$Longitude)
+data$Latitude <- as.numeric(data$Latitude)
+
+data_sp <- SpatialPointsDataFrame(data[,c(6, 7)], 
+                                  data[,-c(6, 7)])
+
+seattle_map <- leaflet(seattle_boundaries) %>% 
+               addTiles() %>% 
+               addPolygons() %>% 
+               addMarkers(data = data, lng = ~Longitude, lat = ~Latitude, 
+                          popup = ~paste("<h3>Details</h3>", "Fatal: ", Fatal, 
+                                         "<br>", "Date: ",  Date, sep = " ")) %>% 
+               setView(lng = -122.335167, lat = 47.608013, zoom = 11,
+                       options = NULL)
+seattle_map
+
+

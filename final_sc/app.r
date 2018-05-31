137d9d180fc2c3f02d5e477040bd201e7be16d69
Salem Gordon <salemg@uw.edu>
2018-05-30 22:24:26 -0700

    Fixed map

--- final_sc/app.r
+++ final_sc/app.r
@@ -110,13 +110,22 @@ server <- function(input, output)
   data_sp <- SpatialPointsDataFrame(data[,c(6, 7)], data[,-c(6, 7)])
   
   output$seattle_map <- renderLeaflet({
-    seattle_map <- leaflet() %>% addTiles() %>%
-                    addMarkers(data = data, lng = ~Longitude, lat = ~Latitude, 
-                               popup = ~paste("<h3>Details</h3>", "Fatal: ", Fatal,
-                                              "<br>", "Date: ",  Date, sep = " "),
-                               clusterOptions = markerClusterOptions()) %>%
-                              setView(lng = -122.335167, lat = 47.608013, zoom = 11, options = NULL)
+    data <- read.csv(file = "../data/SPD_Officer_Involved_Shooting__OIS__Data.csv", 
+                     stringsAsFactors = FALSE)
+    data$Longitude <- as.numeric(data$Longitude)
+    data$Latitude <- as.numeric(data$Latitude)
     
+    data_sp <- SpatialPointsDataFrame(data[,c(6, 7)], 
+                                      data[,-c(6, 7)])
+    
+    seattle_map <- leaflet() %>% 
+      addTiles() %>%
+      addMarkers(data = data, lng = ~Longitude, lat = ~Latitude, 
+                 popup = ~paste("<h3>Details</h3>", "Fatal: ", Fatal, 
+                                "<br>", "Date: ",  Date, sep = " "),
+                 clusterOptions = markerClusterOptions())%>% 
+      setView(lng = -122.335167, lat = 47.608013, zoom = 11,
+              options = NULL)
 })
   
   # chose the 2 dataframes of interest that will be used based on user selection
